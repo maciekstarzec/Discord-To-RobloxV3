30d87e66b0a97b93e41bd0a7b0efd8752ee74665
Elijah King <ekinglijah@gmail.com>
2023-03-12 14:53:00 -0500

    fixing this

--- main.js
+++ main.js
@@ -1,18 +1,10 @@
-// init project
 const crypto = require('crypto');
 const axios = require('axios');
 const https = require('https');
 
 const { Client, EmbedBuilder, GatewayIntentBits, Guild } = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMessageReactions, GatewayIntentBits.MessageContent] });
-
-//// IMPORTANT VVV
-let universeID = 00000; // Your Game Universe ID
-let rolename = 'Admin'; // Admin role that can use the bot
-let prefix = '!'; // Discord bot prefix
-let datastoreApiKey = 'Your Datastore Api Key Here';
-let botToken = 'Your Discord Bot Token Here'; // (Discord bot)
-/// IMPORTANT ^^^
+const { universeID, adminRole, botPrefix, datastoreApiKey, botToken } = require('./Credentials/Config.json');
 
 let numbers = [
     "0️⃣",
@@ -38,7 +30,7 @@ client.on("ready", () => {
 
 const Invalid = new EmbedBuilder()
     .setColor('#eb4034')
-    .setDescription("Invalid user")
+    .setDescription("Invalid user or time specified")
 
 const footer = {
     text: 'Forked by corehimself'
@@ -46,17 +38,12 @@ const footer = {
 
 function byUID(method, usr, message) {
     const Emb = new EmbedBuilder()
-        .setColor('#fff200')
-        //.setTitle(request.headers.username + "'s Data")
-        // .setTitle("Attempt")
-        //.setAuthor('Roblox Error','')
         .setDescription("Attempting to " + method + " UserID " + usr + "...")
         .setTimestamp()
         .setFooter(footer);
-    
+
     message.edit({ embeds: [Emb] });
     https.get("https://api.roblox.com/users/" + usr, (res) => {
-        
         let data = '';
         res.on('data', d => {
             data += d
@@ -64,6 +51,8 @@ function byUID(method, usr, message) {
         res.on('end', () => {
             if (res.statusCode == 200) {
                 toBan.push({ method: method, username: JSON.parse(data).Username, value: usr, cid: message.channel.id, mid: message.id });
+                handleDataResponse(JSON.parse(data).Id, method, message, JSON.parse(data).Username);
+                toBan.shift();
             } else {
                 message.edit({ embeds: [Invalid] });
             }
@@ -73,13 +62,13 @@ function byUID(method, usr, message) {
     });
 }
 
-async function handleDataResponse(userID, method, msg, username) {
+async function handleDataResponse(userID, method, msg, username, sentTime) {
     let entryKey = `user_${userID}`;
-    const JSONValue = await JSON.stringify(method);
-    const ConvertAdd = await crypto.createHash("md5").update(JSONValue).digest("base64")
+    const JSONValue = await JSON.stringify({ method: method, time: sentTime });
+    const ConvertAdd = await crypto.createHash("md5").update(JSONValue).digest("base64");
     const response = await axios.post(
         `https://apis.roblox.com/datastores/v1/universes/${universeID}/standard-datastores/datastore/entries/entry`,
-        method,
+        JSONValue,
         {
             params: {
                 'datastoreName': 'DTRD',
@@ -93,6 +82,7 @@ async function handleDataResponse(userID, method, msg, username) {
         }
     ).catch(err => {
         console.log(err.response.data);
+        console.log(err.message);
     })
 
     if (response && response.data) {
@@ -130,7 +120,9 @@ async function handleDataResponse(userID, method, msg, username) {
     }
 }
 
-function byUser(method, usr, message) {
+// updated byUser function
+function byUser(method, usr, message, banTime) {
+    let tempTime = banTime || "permanent";
     const Emb = new EmbedBuilder()
         .setColor('#fff200')
         //.setTitle(request.headers.username + "'s Data")
@@ -141,19 +133,32 @@ function byUser(method, usr, message) {
         .setFooter(footer);
     message.edit({ embeds: [Emb] });
     https.get("https://api.roblox.com/users/get-by-username?username=" + usr, (res) => {
-        let data = '';
-        res.on('data', d => {
-            data += d
-        })
-        res.on('end', () => {
-            if (JSON.parse(data).Id != undefined) {
-                toBan.push({ method: method, value: JSON.parse(data).Id, username: JSON.parse(data).Username, cid: message.channel.id, mid: message.id });
-                handleDataResponse(JSON.parse(data).Id, method, message, JSON.parse(data).Username);
+    let data = '';
+    res.on('data', d => {
+        data += d;
+    })
+    res.on('end', () => {
+        if (res.statusCode != 200) {
+            message.reply({
+                content: "Too many requests, please try again later",
+                ephemeral: true
+            })
+            return;
+        }
+        try {
+            const jsonData = JSON.parse(data);
+            if (jsonData && jsonData.Id != undefined) {
+                toBan.push({ method: method, value: jsonData.Id, username: jsonData.Username, cid: message.channel.id, mid: message.id });
+                handleDataResponse(jsonData.Id, method, message, jsonData.Username, tempTime);
                 toBan.shift();
             } else {
                 message.edit({ embeds: [Invalid] });
             }
-        });
+        } catch (err) {
+            console.error("Error parsing JSON data", err);
+            message.edit({ embeds: [Invalid] });
+        }
+    });    
     }).on('error', error => {
         console.error("RBLX API (Username) | " + error);
     });
@@ -162,16 +167,16 @@ function byUser(method, usr, message) {
 function isCommand(command, message) {
     var command = command.toLowerCase();
     var content = message.content.toLowerCase();
-    return content.startsWith(prefix + command);
+    return content.startsWith(botPrefix + command);
 }
 
 const TookTooLong = new EmbedBuilder()
     .setColor('#eb4034')
     .setDescription("You took too long to respond!")
 
-async function determineType(method, msg, BotMsg, args) {
+async function determineType(method, msg, BotMsg, args, banTime) {
     if (isNaN(Number(args[1]))) {
-        byUser(method, args[1], BotMsg);
+        byUser(method, args[1], BotMsg, banTime);
     } else {
         const Emb = new EmbedBuilder()
             .setColor('#ea00ff')
@@ -192,7 +197,7 @@ async function determineType(method, msg, BotMsg, args) {
             const filter = (reaction, user) => {
                 return numbers.includes(reaction?.emoji?.name) && user.id === msg.author.id;
             };
-            
+
             BotMsg.awaitReactions({ filter, max: 1, time: 30000, errors: ['time'] })
                 .then(collected => {
                     const reaction = collected.first();
@@ -200,11 +205,11 @@ async function determineType(method, msg, BotMsg, args) {
                         return n == reaction.emoji.name;
                     });
                     BotMsg.reactions.removeAll().catch(error => console.error('Failed to clear reactions: ', error));
-                    
+
                     if (ind == 0) {
-                        byUser(method, args[1], BotMsg);
+                        byUser(method, args[1], BotMsg, banTime);
                     } else if (ind == 1) {
-                        byUID(method, args[1], BotMsg);
+                        byUID(method, args[1], BotMsg, banTime);
                     } else {
                         BotMsg.edit({ embeds: [Invalid] });
                     }
@@ -218,21 +223,60 @@ async function determineType(method, msg, BotMsg, args) {
         }
     }
 }
-      
-    
+
+function timeCheck(time) {
+    let regex = /^(\d+)(s|m|h|d|y)?$/;
+    let matches = regex.exec(time);
+
+    if (matches) {
+        let duration = parseInt(matches[1]);
+        let unit = matches[2];
+        switch (unit) {
+            case "s":
+                duration = duration * 1000;
+                break;
+            case "m":
+                duration = duration * 60 * 1000;
+                break;
+            case "h":
+                duration = duration * 60 * 60 * 1000;
+                break;
+            case "d":
+                duration = duration * 24 * 60 * 60 * 1000;
+                break;
+            case "y":
+                duration = duration * 365 * 24 * 60 * 60 * 1000;
+                break;
+            default:
+                duration = duration * 1;
+        }
+        return new Date(Date.now() + duration);
+    }
+}
 
 client.on('messageCreate', async (message) => {
     if (message.author.bot) return;
-    
-    if (message.member.roles.cache.some(role => role.name === rolename)) {
-        const args = message.content.slice(prefix.length).split(' ');
+
+    if (message.member.roles.cache.some(role => role.name === adminRole)) {
+        const args = message.content.slice(botPrefix.length).split(' ');
         var Emb = new EmbedBuilder()
             .setColor('#eb4034')
             .setDescription("Working...")
-        
+
         if (isCommand("Ban", message)) {
+            var time;
             var BotMsg = await message.channel.send({ embeds: [Emb] });
-            determineType("Ban", message, BotMsg, args);
+            if (!args[2]) {
+                determineType("Ban", message, BotMsg, args);
+                return;
+            }
+
+            time = timeCheck(args[2]);
+            if (!time) {
+                BotMsg.edit({ embeds: [Invalid] });
+                return;
+            }
+            determineType("Ban", message, BotMsg, args, time);
         } else if (isCommand("Unban", message)) {
             var BotMsg = await message.channel.send({ embeds: [Emb] });
             determineType("Unban", message, BotMsg, args);
