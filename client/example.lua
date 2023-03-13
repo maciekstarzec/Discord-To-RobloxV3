30845eb17b60fb0979ba7a511efa9bc4047316aa
Elijah King <ekinglijah@gmail.com>
2023-03-12 23:07:09 -0500

    update

--- client/example.lua
+++ client/example.lua
@@ -1,7 +1,7 @@
 local DTR = require(script.Parent["DTR v3.2.0"])
 
 function onPlayerAdded(plr)
-	DTR.CheckPlayer(plr) -- constantly check
+    DTR.CheckPlayer(plr) -- constantly check
 end
 
 game.Players.PlayerAdded:Connect(onPlayerAdded)
